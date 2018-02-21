0b8768e619caa600aad76b9c765202b6fc91fc4f
JonDuncanson-abertay <32883414+JonDuncanson-abertay@users.noreply.github.com>
2018-02-21 14:43:55 +0000

    First Commit!
    Base input code finished

---
+++ Input.py
@@ -0,0 +1,20 @@
+import pygame
+from pygame.locals import *
+
+class Input():
+    def __init__(self):
+        self.keys = None
+        self.mouseButtons = None
+        self.mousePosition = None
+    def update(self):
+        self.keys = pygame.key.get_pressed()
+        self.mouseButtons = pygame.mouse.get_pressed()
+        self.mousePosition = pygame.mouse.get_pos()
+    def getKeys(self):
+        return self.keys
+    def getMouseButtons(self):
+        return mouseButtons
+    def getMousePosition(self):
+        return mousePosition
+    def getKeyPressed(self, key):
+        return self.keys[key]
