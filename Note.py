709630f963b0faf9b1f0e8ae95d1c468e5a884a6
JonDuncanson-abertay <32883414+JonDuncanson-abertay@users.noreply.github.com>
2018-02-21 16:16:21 +0000

    NoteManager first pass
    Added most of the core functionality to NoteManager

--- Note.py
+++ Note.py
@@ -12,3 +12,41 @@ class BaseObject():
         return self.AABB
     def render(self, screen):
         screen.blit(self.surface, (self.x, self.y))
+    def setX(self, x):
+        self.x = x
+    def setY(self, y):
+        self.y = y
+
+class Note(BaseObject):
+    def __init__(self, x, y, color):
+        BaseObject.__init__(x, y, color)
+        self.isAlive = False
+    def getAlive(self):
+        return self.isAlive
+    def setAlive(self, alive):
+        self.isAlive = alive
+    def setColor(self, color):
+        self.color = color
+    def update(self, dt):
+        if self.isAlive:
+            y -= 0.25 * dt
+
+class NoteManager():
+    def __init__(self):
+        self.y = 0
+        self.Notes = []
+        for count in range(0, 13):
+            self.Notes.append(Note(0, 0, Color(0, 0, 0)))
+    def render(self, screen):
+        for note in self.Notes:
+            note.render(screen)
+    def spawn(self, x, color):
+        for note in self.Notes:
+            if not note.getAlive():
+                note.setX(x)
+                note.setColor(color)
+                note.setAlive(True)
+                return None         
+    def update(self, dt):
+        for note in self.Notes:
+            note.update(dt)           
